d90dccdac21958d8fef0dd0c9fd4144d7769add8
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-03-13 12:33:28 -0500

    controllers folder and getAll Functions

---
+++ controllers/actors.js
@@ -0,0 +1,15 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+  try {
+    const result = await mongodb.getDb().db().collection('actors').find();
+    console.log(result);
+    const lists = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  } catch (error) {
+    console.error('Error retrieving actors:', error);
+    res.status(500).json({ error: 'There was an error while retrieving the actors.' });
+  }
+};
