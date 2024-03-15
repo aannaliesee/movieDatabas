5f0eea033d8b6f4cd76abce819054064472c9b23
jessochs <fac17002@byui.edu>
2024-03-15 14:41:05 -0700

    completed routes

--- routes/directorsRoutes.js
+++ routes/directorsRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const directorControll = require('../controllers/directorsController');
 
 router.get('/', directorControll.getAll);
+router.get('/:id', directorControll.getSingle);
+router.get('/', directorControll.createDirector);
+router.get('/:id', directorControll.updateDirector);
+router.get('/:id', directorControll.deleteDirector);
 
 module.exports = router;
--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const movieControll = require('../controllers/moviesController');
 
 router.get('/', movieControll.getAll);
+router.get('/:id', movieControll.getSingle);
+router.get('/', movieControll.createMovie);
+router.get('/:id', movieControll.updateMovie);
+router.get('/:id', movieControll.deleteMovie);
 
 module.exports = router;
