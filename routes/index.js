145992447bc76e61b9e39a3510c6311727e51eda
jessochs <fac17002@byui.edu>
2024-03-15 12:48:51 -0700

    changed routes in index js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-router.use('/', require('./moviesRoutes'));
-router.use('/', require('./genresRoutes'));
-router.use('/', require('./directorsRoutes'));
-router.use('/', require('./tmoatRoutes'));
+router.use('/moviesController', require('./moviesRoutes'));
+router.use('/genresController', require('./genresRoutes'));
+router.use('/directorsController', require('./directorsRoutes'));
+router.use('/tmoatController', require('./tmoatRoutes'));
 
 module.exports = router
