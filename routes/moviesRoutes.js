ee02e5e315b915b8e3ddeb8d694342e0f5f9fa93
Jaden <jadencamargo@gmail.com>
2024-03-22 15:52:14 -0700

    Movies controller finished and tested locally.

--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const movieControll = require('../controllers/moviesController');
+const { requiresAuth } = require("express-openid-connect");
+
 
 router.get('/', movieControll.getAll);
-// router.get('/:id', movieControll.getSingle);
-// router.get('/', movieControll.createMovie);
-// router.get('/:id', movieControll.updateMovie);
-// router.get('/:id', movieControll.deleteMovie);
+router.get('/:id', movieControll.getMovieById);
+router.post('/', movieControll.postMovie);
+router.put('/:id', movieControll.putMovie);
+router.delete('/:id', movieControll.deleteMovie);
 
 module.exports = router;
