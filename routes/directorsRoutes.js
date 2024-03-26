ebf16a6065e97151e68d9d2d1f015aa4510d9a0c
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2024-03-26 10:50:56 -0500

    adding validation to routes.  cannot test yet.

--- routes/directorsRoutes.js
+++ routes/directorsRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router();
 const directorControll = require('../controllers/directorsController');
+const validate = require('../middleware/validate');
 const { requiresAuth } = require("express-openid-connect");
 
 
 router.get('/', directorControll.getAll);
 router.get('/:id', directorControll.getSingle);
-router.post('/', directorControll.createDirector);
-router.put('/:id', directorControll.updateDirector);
+router.post('/', validate.directorControll.createDirector);
+router.put('/:id', validate.directorControll.updateDirector);
 router.delete('/:id', directorControll.deleteDirector);
 
 module.exports = router;
--- routes/tmoatRoutes.js
+++ routes/tmoatRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router();
 const topMovieControll = require('../controllers/tmoatController');
+const validate = require('../middleware/validate');
 const { requiresAuth } = require("express-openid-connect");
 
 
 router.get('/', topMovieControll.getAll);
 router.get('/:id', topMovieControll.getSingle);
-router.post('/', topMovieControll.createTMOAT);
-router.put('/:id', topMovieControll.updateTMOAT);
+router.post('/', validate.topMovieControll.createTMOAT);
+router.put('/:id', validate.topMovieControll.updateTMOAT);
 router.delete('/:id', topMovieControll.deleteTMOAT);
 
 module.exports = router;
