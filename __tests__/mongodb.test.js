a1bf2290ddb9e9b1c82c1c56997feb056b82e658
jessochs <fac17002@byui.edu>
2024-03-29 14:58:25 -0700

    testing

--- __tests__/mongodb.test.js
+++ __tests__/mongodb.test.js
@@ -1,16 +1,37 @@
 const {MongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
 
 describe('insert', () => {
+  let connection;
+  let db;
 
-    
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = await connection.db('Movies');
+  });
 
-  it('should insert a doc into collection', async () => {
-    const users = db.collection('users');
+  afterAll(async () => {
+    await connection.close();
+  });
 
-    const mockUser = {_id: 'some-user-id', name: 'John'};
+  it('should insert a new user into the collection', async () => {
+    const users = db.collection('movies');
+
+    const mockUser = {
+                       title: 'fire',
+                       directors: 'Penelope', // required, allow both string and array
+                       actors: 'Juliet', // required
+                       genre: 'RomCom',
+                       releaseYear: 2011, // allow only integers between 1800-2025
+                       ratings: 7, // required};
+    }
     await users.insertOne(mockUser);
 
-    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    const insertedUser = await users.findOne({title: "fire"});
     expect(insertedUser).toEqual(mockUser);
   });
 });
