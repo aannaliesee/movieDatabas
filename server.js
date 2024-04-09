3d6dc126b41298a59b3faf036dec2bdd9d88d430
arden <ardenbyui.edu>
2024-04-08 20:04:56 -0600

    auth0 commented in

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const { auth, requiresAuth } = require("express-openid-connect");
 const port = process.env.PORT || 8080;
 const app = express();
 
-// authentication
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: process.env.SECRET,
-//   baseURL: process.env.BASE_URL,
-//   clientID: process.env.CLIENT_ID,
-//   issuerBaseURL: process.env.ISSUER_BASE_URL,
-// };
+authentication
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
-// // auth router attaches /login, /logout, and /callback routes to the baseURL
-// app.use(auth(config)); 
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config)); 
 
-// app.get("/", (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
-// });
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 
 mongodb.initDb((err) => {
   if (err) {
